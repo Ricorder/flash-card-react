880fc595a664330048635370cf8b8a5a4ea2c170
Evgeny Kolesnikov <48915901+Ricorder@users.noreply.github.com>
2021-04-30 13:51:44 +0300

    Fix back

--- server/app.js
+++ server/app.js
@@ -3,20 +3,15 @@ const app = express();
 const logger = require('morgan');
 const fs = require('fs');
 const cors = require('cors');
-// const path = require('path');
-// const hbs = require('hbs');
 
 const themeCardRouter = require('./routes/themeCardRouter');
 const userRouter = require('./routes/userRouter');
 
-// app.set('view engine', 'hbs');
-// app.set('views', path.join(__dirname, 'views'));
-
 app.use(logger('dev'));
 app.use(logger('common', {stream: fs.createWriteStream('./access.log', {flags: 'a'})}));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-// app.use(express.static(path.join(__dirname, 'public')))
+app.use(cors);
+// app.use(express.urlencoded({extended: true}));
 
 app.use('/', themeCardRouter);
 app.use('/user', userRouter);
